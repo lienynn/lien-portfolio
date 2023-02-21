e29326cd24f9ab0672916c7d0125601e51d4a140
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-02-21 10:46:30 -0500

    removed contact component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,3 @@
-// import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
 import React from "react";
 import { skills } from "../data";
 
@@ -6,8 +5,7 @@ export default function Skills() {
   return (
     <section id="skills">
       <div className="container px-5 py-10 mx-auto">
-        <div className="text-center mb-20">
-          {/* <ChipIcon className="w-10 inline-block mb-4" /> */}
+        <div className="text-center mb-20"> 
           <h1 className="sm:text-4xl text-3xl font-medium title-font text-black mb-4">
             Skills &amp; Technologies
           </h1>
@@ -20,7 +18,6 @@ export default function Skills() {
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
               <div className="bg-indigo-300 rounded flex p-4 h-full items-center">
-                {/* <BadgeCheckIcon className="text-white w-6 h-6 flex-shrink-0 mr-4" /> */}
                 <span className="title-font font-medium text-white">
                   {skill}
                 </span>
