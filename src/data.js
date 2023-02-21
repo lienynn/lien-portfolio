ceaa80f2fd0d2fd1ffbb9d1da13957e9d967bcb0
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-02-21 11:36:53 -0500

    added data to data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
       title: "Job Board Page",
-      stack: "React",
+      stack: "React,Javascript, Netlify",
       description:
         "This is a simple mockup of a job board page built using React. It has a filtering function that filters jobs based on a particular tech stack or position.",
       image: "./jobboard.gif",
@@ -10,7 +10,7 @@ export const projects = [
   
     {
       title: "BrunchTime",
-      stack: "RoR, Javascript, Stimulus, Bootstrap, Mapbox API, PostgreSQL",
+      stack: "RoR, Javascript, Stimulus, Bootstrap, Mapbox API, PostgreSQL, Heroku",
       description:
         "BrunchTime mobile app helps you find the best brunch place with the shortest wait-time",
       image: "./brunchtime.gif",
@@ -19,7 +19,7 @@ export const projects = [
   
     {
       title: "FAQ Accordian",
-      stack: "React",
+      stack: "React, Javascript, Netlify",
       description:
         "Simple accordion showcasing toggling state, conditionals (&&), passing data to components via props and displaying multiple components with .map()",
       image: "./accordion.gif",
@@ -28,7 +28,7 @@ export const projects = [
   
     {
       title: "Basic Digital Counter",
-      stack: "React",
+      stack: "React, Javascript, Netlify",
       description:
         "Basic React Counter using Props and useState",
       image: "./counter.gif",
