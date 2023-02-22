a4063a95995dac3ca994489b8b12c05119972748
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-02-22 15:45:47 -0500

    added highlight to key words using RoughNotation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,22 +4,22 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function About() {
 
-
-
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
         <h1 className="title-font sm:text-4xl text-2xl mb-4 font-medium text-black">
             Hi! I'm Lien, a 
-            <RoughNotation type="highlight" show={true} color="#F3b8d7" animated="true" animationDuration="1000"> fullstack developer </RoughNotation> 
+            <RoughNotation type="highlight" show={true} color="#F3b8d7" animated="true"> fullstack developer </RoughNotation> 
             based in Montreal.
         </h1>
           <h2 className="title-font sm:text-2xl text-2xl mb-4 font-medium text-black">
-            I'm passionate about telling stories through code and design and love building thoughtful, user-friendly minimalistic applications.
+            I'm passionate about telling 
+            <RoughNotation type="highlight" show={true} color="#B8dbf3" animated="true">stories through code and design</RoughNotation>
           </h2>
+          <h2 className="title-font sm:text-2xl text-2xl mb-4 font-medium text-black"> and love building thoughtful, user-friendly minimalistic applications.</h2>
           <p className="mb-8 leading-relaxed">
-          I have an acuity for problem solving and as a lifelong learner - I am eager to continuously grow my skills in order to keep up to date with the fast-changing trends within the industry and further my skillset 🚀 👩🏻‍💻
+          I have an <RoughNotation type="highlight" show={true} color="#DAF7BE" animated="true"> acuity for problem solving</RoughNotation> and as a lifelong learner - I am eager to continuously grow my skills in order to keep up to date with the fast-changing trends within the industry and further my skillset 🚀 👩🏻‍💻
           </p>
           <div className="flex justify-center">
             <a
